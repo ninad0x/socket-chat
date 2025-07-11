b3637f1f4a27c081bc0e2668032ed386a4a55ec7
Ninad09 <ninadgaonkar234@gmail.com>
2025-07-11 23:33:52 +0530

    send message completed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,50 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({port: 8080})
+const wss = new WebSocketServer({port: 8080});
+
+interface User {
+    socket: WebSocket;
+    room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", function(socket) {
     console.log("User Connected")
 
-    socket.on("message", (e) => {
-        if (e.toString() === "ping") {
-            socket.send("pong")
+    socket.on("message", (message: string) => {
+
+        const parsedMessage = JSON.parse(message)
+
+        console.log(parsedMessage.payload.message);
+        
+
+        if (parsedMessage.type === "join") {
+
+            allSockets.push({
+                socket: socket,
+                room: parsedMessage.payload.roomId
+            })
+            
+            console.log("users are  ",allSockets);
+            
         }
+
+        if (parsedMessage.type === "chat") {
+            const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room
+
+            // console.log("currentUserRoom  ", currentUserRoom);
+            
+            // allSockets.forEach((x) => x.socket.send(parsedMessage.payload.message))
+
+            // @ts-ignore
+            // console.log("result  ",
+             allSockets
+                .filter((x) => x.room == currentUserRoom)   // gives all red
+                .forEach((x) => x.socket.send(parsedMessage.payload.message))   // sends msg   
+            // )
+        }
+
+
     })
 })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,63 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useState} from 'react'
 
 import './App.css'
 
 function App() {
 
-  useEffect(() => {
-    // const ws = new WebSocket("ws://localhost:8080")
-  }, []);
+  const [messages, setMessages] = useState(["Hi there"]);
+  const wsRef = useRef<WebSocket>(null);
+  const inputRef = useRef<HTMLButtonElement>()
+
 
   function sendMessage() {
 
+    if (inputRef.current.value === "") return
+    
+    const message = inputRef.current.value;
+    wsRef.current?.send(
+      JSON.stringify({
+        type: "chat",
+        payload: {
+          message: message
+        }
+      })
+    )
+    inputRef.current.value = ""
   }
 
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080")
+    ws.onmessage = (e) => {
+      setMessages(m => [...m, e.data])
+    }
+    wsRef.current = ws;
+
+    ws.onopen = () => {
+      ws.send(JSON.stringify({
+        type: "join",
+        payload: {
+          roomId: "red"
+        }
+      }))
+    }
+
+    return () => {
+      ws.close()
+  }
+    
+  }, [])
+
   return (
-    <div>
-      <input type="text" placeholder="message..."/>
-      <button onClick={sendMessage}>Send</button>
+    <div className='h-screen bg-gray-400 justify-between flex flex-col p-4'>
+      <div>
+        {messages.map(e => 
+        <div className='bg-white p-2 mb-1 rounded-lg w-fit'>{e}</div>
+        )}
+      </div>
+      <div>
+        <input ref={inputRef} className='bg-white px-8 py-4 rounded-l-2xl' type="text" placeholder="message..."/>
+        <button className='px-8 py-4 bg-violet-500 hover:bg-violet-600 cursor-pointer rounded-r-2xl text-white' onClick={sendMessage}>Send</button>
+      </div>
     </div>
   )
 }
